08db5c9e6a4859b611627f5485bd1ebdd52392a6
Alistair Muldal <alimuldal@gmail.com>
2014-10-06 14:33:24 +0100

    function for creating fake movies w. Gaussian spatial filters + offset background

--- pyfnnd/demo.py
+++ pyfnnd/demo.py
@@ -41,6 +41,49 @@ def make_fake_data(ncells, nframes, dt=(1. / 50), rate=0.5, tau=1.,
 
     return (A.squeeze() for A in (S, C, F))
 
+def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
+                    bg_intensity=0.1, mask_sigma=30, dt=(1. / 50), rate=0.5,
+                    tau=1., sigma=0.8):
+
+    # poisson spikes
+    S = stats.poisson.rvs(rate * dt, size=nframes)
+
+    # internal calcium dynamics
+    nk = int(10 * tau / dt)
+    t = np.arange(nk) * dt
+    kernel = np.exp(-t / tau)
+    C = ndimage.convolve1d(S.astype(np.float64), kernel, axis=0,
+                           origin=(-nk / 2))
+
+    # pixel weights (sum to 1)
+    nr, nc = mask_shape
+    if mask_center is None:
+        mask_center = (nc // 2., nr // 2.)
+    a, b = mask_center
+    y, x = np.ogrid[:nr, :nc]
+    xs = (x - a) ** 2.
+    ys = (y - b) ** 2.
+    twoss = 2. * mask_sigma ** 2.
+    mask = (1. / (twoss * np.pi)) * np.exp(-1 * ((xs / twoss) + (ys / twoss)))
+
+    # background fluorescence
+    background_fluor = np.random.randn(nr, nc) * bg_intensity
+
+    gamma = np.exp(-dt / tau)
+    alpha = mask.ravel()
+    beta = background_fluor.ravel()
+    lamb = rate
+
+    # spatially & temporally white noise
+    epsilon = np.random.normal(loc=0., scale=sigma, size=C.shape)
+
+    # simulated fluorescence
+    F = C[None, :] * alpha[:, None] + beta[:, None] + epsilon
+
+    theta = (sigma, alpha, beta, lamb, gamma)
+
+    return F, C, S, theta
+
 
 def make_demo_plots():
 
