833bc3fe1cfc44a4fca9c41a84c8933ea454406c
Alistair Muldal <alimuldal@gmail.com>
2014-10-09 16:29:56 +0100

    changed the way the fake data is generated (use scipy.signal.lfilter instead of manually constructing kernel and convolving it with spikes using scipy.ndimage.convolve1d). seems more correct (C starts at zero). also make_fake_data() now returns theta as well for consistency

--- pyfnnd/demo.py
+++ pyfnnd/demo.py
@@ -1,12 +1,12 @@
 import numpy as np
-from scipy import stats, ndimage
+from scipy import stats, signal
 from _fnndeconv import deconvolve
 
 
 def make_fake_data(ncells, nframes, dt=(1. / 50), rate=0.5, tau=1.,
-                   sigma=0.2):
+                   sigma=0.2, alpha=1., beta=0.):
     """
-    Generate fake fluorescence traces
+    Generate 1D fake fluorescence traces
 
     Arguments:
     ---------------------------------------------------------------------------
@@ -16,44 +16,66 @@ def make_fake_data(ncells, nframes, dt=(1. / 50), rate=0.5, tau=1.,
         rate:       spike rate (Hz)
         tau:        time constant of decay in calcium concentration (s)
         sigma:      SD of additive noise on fluorescence
+        alpha:      scaling parameter for fluorescence modulation
+        beta:       baseline fluorescence
 
     Returns:
     ---------------------------------------------------------------------------
-        S:          spike counts
-        C:          calcium concentration
-        F:          simulated fluorescence
+        F:          fluorescence [ncells, nframes]
+        C:          calcium concentration [ncells, nframes]
+        n:          spike train [ncells, nframes]
+        theta:      tuple of true model parameters [5,]
 
-    Each of the outputs are (ncells, nframes) arrays
     """
 
     # poisson spikes
-    S = stats.poisson.rvs(rate * dt, size=(ncells, nframes))
+    n = stats.poisson.rvs(rate * dt, size=(ncells, nframes))
 
     # internal calcium dynamics
-    nk = int(10 * tau / dt)
-    t = np.arange(nk) * dt
-    kernel = np.exp(-t / tau)
-    C = ndimage.convolve1d(S.astype(np.float64), kernel, axis=1,
-                           origin=-nk / 2)
+    gamma = np.exp(-dt / tau)
+    C = signal.lfilter(np.r_[1], np.r_[1, -gamma], n, axis=1)
 
     # noise
     F = C + np.random.normal(loc=0., scale=sigma, size=C.shape)
 
-    return (A.squeeze() for A in (S, C, F))
+    lamb = rate
+    theta = (sigma, alpha, beta, lamb, gamma)
+
+    return F, C, n, theta
 
 def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
                     bg_intensity=0.1, mask_sigma=30, dt=(1. / 50), rate=0.5,
                     tau=1., sigma=0.8):
+    """
+    Generate 2D fake fluorescence movie
+
+    Arguments:
+    ---------------------------------------------------------------------------
+        nframes:        number of timebins to simulate
+        mask_shape:     tuple (nrows, ncols), shape of a single movie frame
+        mask_center:    tuple (x, y), pixel coords of cell center
+        bg_intensity:   scalar, amplitude of (static) baseline fluorescence
+        mask_sigma:     scalar, standard deviation of Gaussian mask
+        dt:             timestep (s)
+        rate:           spike rate (Hz)
+        tau:            time constant of decay in calcium concentration (s)
+        sigma:          SD of additive noise on fluorescence
+
+    Returns:
+    ---------------------------------------------------------------------------
+        F:          fluorescence [npixels, nframes]
+        C:          calcium concentration [nframes,]
+        n:          spike train [nframes,]
+        theta:      tuple of true model parameters [5,]
+
+    """
 
     # poisson spikes
-    S = stats.poisson.rvs(rate * dt, size=nframes)
+    n = stats.poisson.rvs(rate * dt, size=nframes)
 
     # internal calcium dynamics
-    nk = int(10 * tau / dt)
-    t = np.arange(nk) * dt
-    kernel = np.exp(-t / tau)
-    C = ndimage.convolve1d(S.astype(np.float64), kernel, axis=0,
-                           origin=(-nk / 2))
+    gamma = np.exp(-dt / tau)
+    C = signal.lfilter(np.r_[1], np.r_[1, -gamma], n, axis=0)
 
     # pixel weights (sum to 1)
     nr, nc = mask_shape
@@ -69,7 +91,6 @@ def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
     # background fluorescence
     background_fluor = np.random.randn(nr, nc) * bg_intensity
 
-    gamma = np.exp(-dt / tau)
     alpha = mask.ravel()
     beta = background_fluor.ravel()
     lamb = rate
@@ -82,41 +103,41 @@ def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
 
     theta = (sigma, alpha, beta, lamb, gamma)
 
-    return F, C, S, theta
+    return F, C, n, theta
 
 
-def make_demo_plots():
+# def make_demo_plots():
 
-    np.random.seed(0)
+#     np.random.seed(0)
 
-    s, c, f = make_fake_data(1, 10000)
-    n_best, c_best, ll_best, theta_best = deconvolve(f, verbosity=1)
+#     s, c, f = make_fake_data(1, 10000)
+#     n_best, c_best, ll_best, theta_best = deconvolve(f, verbosity=1)
 
-    try:
-        import matplotlib
-        from matplotlib import pyplot as plt
-        plt.ion()
-    except ImportError:
-        print "matplotlib is required for making the demo plots"
-        return
+#     try:
+#         import matplotlib
+#         from matplotlib import pyplot as plt
+#         plt.ion()
+#     except ImportError:
+#         print "matplotlib is required for making the demo plots"
+#         return
 
-    fig, ax = plt.subplots(2, 1, sharex=True)
-    for aa in ax:
-        aa.hold(True)
+#     fig, ax = plt.subplots(2, 1, sharex=True)
+#     for aa in ax:
+#         aa.hold(True)
 
-    t = np.arange(10000) * 0.02
+#     t = np.arange(10000) * 0.02
 
-    real, = ax[0].plot(t, f, 'b')
-    inferred, = ax[0].plot(t, c_best + theta_best[1], 'r')
-    ax[0].set_ylabel('Fluorescence')
+#     real, = ax[0].plot(t, f, 'b')
+#     inferred, = ax[0].plot(t, c_best + theta_best[1], 'r')
+#     ax[0].set_ylabel('Fluorescence')
 
-    ax[1].plot(t, s, 'b')
-    ax[1].plot(t, n_best, 'r')
+#     ax[1].plot(t, s, 'b')
+#     ax[1].plot(t, n_best, 'r')
 
-    ax[1].set_xlabel('Time (sec)')
-    ax[1].set_ylabel('Spikes')
+#     ax[1].set_xlabel('Time (sec)')
+#     ax[1].set_ylabel('Spikes')
 
-    plt.show()
+#     plt.show()
 
 if __name__ == "__main__":
     make_demo_plots()
