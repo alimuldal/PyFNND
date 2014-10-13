d8539bb17e366722708569b445ec794ddf39c026
Alistair Muldal <alimuldal@gmail.com>
2014-10-13 18:29:03 +0100

    removed make_fake_data() - use make_fake_movie(..., mask_shape=(1, 1)) to get 1D data. simplified code for generating demo plots.

--- pyfnnd/demo.py
+++ pyfnnd/demo.py
@@ -1,54 +1,11 @@
 import numpy as np
-from scipy import stats, signal
+from scipy import signal
 from _fnndeconv import deconvolve
+import plotting
 
-
-def make_fake_data(ncells, nframes, dt=(1. / 50), rate=0.5, tau=1.,
-                   sigma=0.2, alpha=1., beta=0., seed=None):
-    """
-    Generate 1D fake fluorescence traces
-
-    Arguments:
-    ---------------------------------------------------------------------------
-        ncells:     number of traces to generate
-        nframes:    number of timebins to simulate
-        dt:         timestep (s)
-        rate:       mean spike rate (Hz)
-        tau:        time constant of decay in calcium concentration (s)
-        sigma:      SD of additive noise on fluorescence
-        alpha:      scaling parameter for fluorescence modulation
-        beta:       baseline fluorescence
-
-    Returns:
-    ---------------------------------------------------------------------------
-        F:          fluorescence [ncells, nframes]
-        C:          calcium concentration [ncells, nframes]
-        n:          spike train [ncells, nframes]
-        theta:      tuple of true model parameters [5,]
-
-    """
-
-    gen = np.random.RandomState(seed)
-
-    # poisson spikes
-    n = gen.poisson(rate * dt, size=(ncells, nframes))
-
-    # internal calcium dynamics
-    gamma = np.exp(-dt / tau)
-    C = signal.lfilter(np.r_[1], np.r_[1, -gamma], n, axis=1)
-
-    # noise
-    F = C + gen.randn(*C.shape) * sigma
-
-    lamb = rate
-    theta = (sigma, alpha, beta, lamb, gamma)
-
-    return F, C, n, theta
-
-
-def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
-                    bg_intensity=0.1, mask_sigma=30, dt=(1. / 50), rate=0.5,
-                    tau=1., sigma=0.8, seed=None):
+def make_fake_movie(nframes, mask_shape=(64, 64), mask_center=None,
+                    bg_intensity=0.1, mask_sigma=10, dt=0.02, rate=1.0,
+                    tau=1., sigma=0.001, seed=None):
     """
     Generate 2D fake fluorescence movie
 
@@ -63,13 +20,15 @@ def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
         rate:           mean spike rate (Hz)
         tau:            time constant of decay in calcium concentration (s)
         sigma:          SD of additive noise on fluorescence
+        seed:           Seed for RNG
 
     Returns:
     ---------------------------------------------------------------------------
         F:          fluorescence [npixels, nframes]
         C:          calcium concentration [nframes,]
         n:          spike train [nframes,]
-        theta:      tuple of true model parameters [5,]
+        theta:      tuple of true model parameters:
+                    (sigma, alpha, beta, lambda, gamma)
 
     """
 
@@ -95,22 +54,6 @@ def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
     alpha = np.exp(-1 * ((xs / twoss) + (ys / twoss))).ravel()
     alpha /= alpha.sum()
 
-    # mask = np.array([
-    #     [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-    #     [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    #     [0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
-    #     [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    #     [0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
-    #     [0, 1, 0, 0, 1, 1, 0, 0, 1, 0],
-    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-    #     [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #     ]).astype(np.float64)
-    # alpha = mask.ravel()
-    # alpha /= alpha.sum()
-    # npix = alpha.size
-
     # background fluorescence
     beta = gen.randn(npix) * bg_intensity
 
@@ -128,38 +71,27 @@ def make_fake_movie(nframes, mask_shape=(256, 256), mask_center=None,
     return F, C, n, theta
 
 
-# def make_demo_plots():
+def make_demo_plots():
 
-#     np.random.seed(0)
+    # single pixel
+    F, C, n, theta = make_fake_movie(1000, dt=0.02, mask_shape=(1, 1),
+                                     sigma=0.1, seed=0)
+    n_best, C_best, LL, theta_best = deconvolve(
+        F, dt=0.02, verbosity=1, learn_theta=(0, 1, 1, 0, 0),
+        spikes_tol=1E-3, params_tol=1E-6,
+    )
 
-#     s, c, f = make_fake_data(1, 10000)
-#     n_best, c_best, ll_best, theta_best = deconvolve(f, verbosity=1)
+    plotting.ground_truth_1D(F, n_best, C_best, theta_best, n, C, theta, 0.02)
 
-#     try:
-#         import matplotlib
-#         from matplotlib import pyplot as plt
-#         plt.ion()
-#     except ImportError:
-#         print "matplotlib is required for making the demo plots"
-#         return
-
-#     fig, ax = plt.subplots(2, 1, sharex=True)
-#     for aa in ax:
-#         aa.hold(True)
-
-#     t = np.arange(10000) * 0.02
-
-#     real, = ax[0].plot(t, f, 'b')
-#     inferred, = ax[0].plot(t, c_best + theta_best[1], 'r')
-#     ax[0].set_ylabel('Fluorescence')
-
-#     ax[1].plot(t, s, 'b')
-#     ax[1].plot(t, n_best, 'r')
-
-#     ax[1].set_xlabel('Time (sec)')
-#     ax[1].set_ylabel('Spikes')
-
-#     plt.show()
+    # 2D movie
+    F, C, n, theta = make_fake_movie(1000, dt=0.02, mask_shape=(64, 64),
+                                     sigma=0.001, seed=1)
+    n_best, C_best, LL, theta_best = deconvolve(
+        F, dt=0.02, verbosity=1, learn_theta=(0, 1, 1, 0, 0),
+        spikes_tol=1E-3, params_tol=1E-6,
+    )
+    plotting.ground_truth_2D(F, n_best, C_best, theta_best, n, C, theta, 0.02,
+                             64, 64)
 
 if __name__ == "__main__":
     make_demo_plots()
