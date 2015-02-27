57f75b94c9167cca687d6bce1160e230c90fe3a5
Alistair Muldal <alimuldal@gmail.com>
2015-02-27 14:05:43 +0000

    reload modules in __init__.py

--- pyfnnd/__init__.py
+++ pyfnnd/__init__.py
@@ -1,5 +1,11 @@
-from _fnndeconv import deconvolve
+import _fnndeconv
 import demo
+
+reload(_fnndeconv)
+reload(demo)
+
+from _fnndeconv import deconvolve
+
 try:
     from _fnndeconv import apply_all_cells
 except ImportError:
