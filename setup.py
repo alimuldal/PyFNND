3e2797fed7f729cd97d49a7498b70d1d1f5ea1f7
Alistair Muldal <alimuldal@gmail.com>
2016-03-19 18:12:34 +0000

    increment minor version, change contact email address

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='pyfnnd',
     author='Alistair Muldal',
-    version='0.3',
-    author_email='alistair.muldal@pharm.ox.ac.uk',
+    version='0.4',
+    author_email='alimuldal@gmail.com',
     url='https://github.com/alimuldal/PyFNND',
     description='A Python implementation of fast non-negative deconvolution',
     packages=['pyfnnd'],
