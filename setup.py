3cbe0622a385f5206837bfd944d781aa7b1649ea
Alistair Muldal <alimuldal@gmail.com>
2017-12-06 01:49:51 +0000

    Bump the minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyfnnd',
     author='Alistair Muldal',
-    version='0.4',
+    version='0.5',
     author_email='alimuldal@gmail.com',
     url='https://github.com/alimuldal/PyFNND',
     description='A Python implementation of fast non-negative deconvolution',
